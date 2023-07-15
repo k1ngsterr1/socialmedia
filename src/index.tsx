b5dc668967a90a8b3a131514bbbf875522465bd3
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-16 00:27:24 +0600

    SocialNetworkProject

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV28bf3A8whHJ7N7Got6RrXCKhHAPYjYU",
+  authDomain: "studyapp-90324.firebaseapp.com",
+  projectId: "studyapp-90324",
+  storageBucket: "studyapp-90324.appspot.com",
+  messagingSenderId: "440148036708",
+  appId: "1:440148036708:web:eb636de1a68188b393e371",
+  measurementId: "G-EK7YKTGEM6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Authentication.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authentication = () => {
+  return <div className="authentication-page">Authentication</div>;
+};
+
+export default Authentication;
