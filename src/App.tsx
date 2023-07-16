f069a0aa0622d42c485a6b527d5b3ee05a69b251
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-16 15:03:12 +0600

    SNP2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Authentication from "./pages/Authentication";
 
 const App = () => {
-  return <div className="application"></div>;
+  return (
+    <div className="application">
+      <Authentication />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Authentication = () => {
-  return <div className="authentication-page">Authentication</div>;
+  return (
+    <div className="page bg-teal-50">
+      <div className="container">
+        <h1 className="h1">Authentication</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Authentication;
