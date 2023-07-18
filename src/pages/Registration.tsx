858c94f60df837b88b3bc162c5765f93d6f3b9fa
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-07-18 18:26:04 +0600

    SNP9

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -7,32 +7,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-// const app = initializeApp(firebaseConfig);
-
-// const validationSchema = Yup.object({
-//   name: Yup.string().required("Name is required"),
-//   email: Yup.string().email("Invalid email").required("Email is required"),
-//   password: Yup.string()
-//     .required("Password is required")
-//     .min(8, "Password must be at least 8 characters")
-//     .matches(
-//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-//       "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-//     ),
-//   confirmPassword: Yup.string()
-//     .test("password-match", "Passwords must match", function (value) {
-//       return value === this.resolve(Yup.ref("password"));
-//     })
-//     .nullable()
-//     .required("Confirm Password is required"),
-// });
-
 const Registration: React.FC = () => {
-  const styles = {
-    "--left-px-value": "150px",
-    "--top-px-value": "80px",
-  };
-
   const initialValues = {
     name: "",
     email: "",
@@ -94,70 +69,88 @@ const Registration: React.FC = () => {
           validationSchema={validationSchema}
           onSubmit={registrationSubmit}
         >
-          <Form className="flex flex-col m-auto" autoComplete="off">
-            <div className="flex mt-16 items-center justify-center ">
-              <Field
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Username"
-                className="w-64 h-12 text-gray-900 pl-4 outline-none border-solid border-2 border-teal-400 rounded focus:border-teal-600"
-              />{" "}
-              <ErrorMessage
-                name="name"
-                component="p"
-                className="text-red-600 font-bold text-lg flex align-center"
-              />
-            </div>
-            <div className="flex">
-              <Field
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                className="w-64 h-12 mt-4 text-gray-900 pl-4 outline-none border-solid border-2 border-teal-400 rounded focus:border-teal-600"
-              />
-            </div>
-            <div className="flex">
-              <Field
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                className="w-64 h-12 mt-4 text-gray-900 pl-4 outline-none border-solid border-2 border-teal-400 rounded focus:border-teal-600"
-              />
-            </div>
-            <div className="flex">
-              <Field
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                placeholder="Confirm Password"
-                className="w-64 h-12 mt-4 text-gray-900 pl-4 outline-none border-solid border-2 border-teal-400 rounded focus:border-teal-600"
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 w-32 h-12 cursor-pointer mt-8 m-auto text-white text-xl rounded hover:bg-blue-700 transition-colors duration-300"
-            >
-              Register
-            </button>
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="text-red-600 font-bold text-lg mt-4"
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="text-red-600 font-bold text-lg mt-4"
-            />
-            <ErrorMessage
-              name="confirmPassword"
-              component="div"
-              className="text-red-600 font-bold text-lg mt-4"
-            />
-          </Form>
+          {({ errors, touched }) => (
+            <Form className="flex flex-col m-auto" autoComplete="off">
+              <div className="flex mt-16 items-center justify-center flex-col ">
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Username"
+                  className={`w-64 h-12 text-gray-900 pl-4 outline-none border-solid border-2 rounded focus:border-teal-600 ${
+                    errors.name && touched.name
+                      ? "border-red-500"
+                      : "border-teal-400"
+                  }`}
+                />{" "}
+                <ErrorMessage
+                  name="name"
+                  component="p"
+                  className="text-red-600 font-bold text-lg flex align-center mt-4"
+                />
+              </div>
+              <div className="flex mt-4 items-center justify-center flex-col ">
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  className={`w-64 h-12 text-gray-900 pl-4 outline-none border-solid border-2 rounded focus:border-teal-600 ${
+                    errors.name && touched.name
+                      ? "border-red-500"
+                      : "border-teal-400"
+                  }`}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className="text-red-600 font-bold text-lg flex align-center mt-4"
+                />
+              </div>
+              <div className="flex mt-4 items-center justify-center flex-col ">
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  className={`w-64 h-12 text-gray-900 pl-4 outline-none border-solid border-2 rounded focus:border-teal-600 ${
+                    errors.name && touched.name
+                      ? "border-red-500"
+                      : "border-teal-400"
+                  }`}
+                />
+                <ErrorMessage
+                  name="password"
+                  component="p"
+                  className="text-red-600 font-bold text-lg flex align-center mt-4"
+                />
+              </div>
+              <div className="flex mt-4 items-center justify-center flex-col ">
+                <Field
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  className={`w-64 h-12 text-gray-900 pl-4 outline-none border-solid border-2 rounded focus:border-teal-600 ${
+                    errors.name && touched.name
+                      ? "border-red-500"
+                      : "border-teal-400"
+                  }`}
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="p"
+                  className="text-red-600 font-bold text-lg flex align-center mt-4"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 w-32 h-12 cursor-pointer mt-8 m-auto text-white text-xl rounded hover:bg-blue-700 transition-colors duration-300"
+              >
+                Register
+              </button>
+            </Form>
+          )}
         </Formik>
       </div>
     </div>
